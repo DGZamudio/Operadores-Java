5789dc24ab0fce2e1f1f4148834f3a8cb63081b4
AndresO <ochoaperezandreso1@gmail.com>
2024-02-27 09:16:43 -0500

    Primera Version

---
+++ Problemas-Java-main/src/main/java/co/edu/sena/Miscelanea/Ejercicios.java
@@ -0,0 +1,32 @@
+package co.edu.sena.Miscelanea;
+
+public class Ejercicios 
+{
+    public double areaTriangulo(double d1, double d2){
+        return (d1*d2) / 2;
+    }
+    public double SumaDeDosNum(double d1, double d2){
+        return d1+d2;
+    }
+    public double NumeroAlCuadrado(double d1){
+        return d1*d1;
+    }
+    public double EuroADolar(double d1){
+        return d1*1.08;
+    }
+    public double AreaYPerimetroCuadrado(double d1){
+        return d1 * d1;
+        return d1*4;
+    }
+    public double AreaYVolumenCilindro(double d1, double d2){
+        return 2*3.14*d1*(d1 + d2);
+        return (3.14 * (d1 * d1)) * d2;
+    }
+    public double AreaYLongitudCirc(double d1){
+        return 3.14 * ( d1 * d1 );
+        return 2*3.14*d1;
+    }
+    public double PromedioTresNumeros(double d1, double d2, double d3){
+        return (d1+d2+d3)/3;
+    }
+}
