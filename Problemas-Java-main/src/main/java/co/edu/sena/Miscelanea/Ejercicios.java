0641688921d9bee959062b1b83851d29d56b46b1
DGZamudio <dmzamudio80@soy.sena.edu.co>
2024-02-27 10:26:30 -0500

    Version Oficial

--- Problemas-Java-main/src/main/java/co/edu/sena/Miscelanea/Ejercicios.java
+++ Problemas-Java-main/src/main/java/co/edu/sena/Miscelanea/Ejercicios.java
@@ -2,31 +2,38 @@ package co.edu.sena.Miscelanea;
 
 public class Ejercicios 
 {
-    public double areaTriangulo(double d1, double d2){
+    public static double areaTriangulo(double d1, double d2){
         return (d1*d2) / 2;
     }
-    public double SumaDeDosNum(double d1, double d2){
+    public static double SumaDeDosNum(double d1, double d2){
         return d1+d2;
     }
-    public double NumeroAlCuadrado(double d1){
+    public static double NumeroAlCuadrado(double d1){
         return d1*d1;
     }
-    public double EuroADolar(double d1){
+    public static double EuroADolar(double d1){
         return d1*1.08;
     }
-    public double AreaYPerimetroCuadrado(double d1){
+    public static double AreaCuadrado(double d1){
         return d1 * d1;
+    }
+    public static double PerimetroCuadrado(double d1){
         return d1*4;
     }
-    public double AreaYVolumenCilindro(double d1, double d2){
+    public static double AreaCilindro(double d1, double d2){
         return 2*3.14*d1*(d1 + d2);
+    }
+    public static double VolumenCilindro(double d1,double d2){
         return (3.14 * (d1 * d1)) * d2;
     }
-    public double AreaYLongitudCirc(double d1){
+    public static double AreaCirc(double d1){
         return 3.14 * ( d1 * d1 );
+    }
+    public static double LongitudCirc(double d1){
+
         return 2*3.14*d1;
     }
-    public double PromedioTresNumeros(double d1, double d2, double d3){
+    public static double PromedioTresNumeros(double d1, double d2, double d3){
         return (d1+d2+d3)/3;
     }
 }
--- Problemas-Java-main/src/main/java/co/edu/sena/Miscelanea/Main.java
+++ Problemas-Java-main/src/main/java/co/edu/sena/Miscelanea/Main.java
@@ -5,102 +5,102 @@ public class Main {
     public static void main( String[] args )
     {
         Scanner src = new Scanner (System.in);
-        double d1;
-        double d2;
-        double d3;
+        double Num1;
+        double Num2;
+        double Num3;
         double result;
     
         System.out.println( "Problema 1:Area de un triangulo." );
         System.out.println( "Ingresa la base:" );
-        d1 = src.nextDouble();
+        Num1 = src.nextDouble();
     
         System.out.println( "Ingresa la altura:" );
-        d2 = src.nextDouble();
+        Num2 = src.nextDouble();
     
-        result = (Ejercicios.areaTriangulo()) ;
+        result = Ejercicios.areaTriangulo(Num1,Num2) ;
     
         System.out.println( "El area del triangulo es:" + result);
     
         System.out.println( "Problema 2:Suma de dos numeros." );
         System.out.println( "Ingresa el primer numero:" );
-        d1 = src.nextDouble();
+        Num1 = src.nextDouble();
     
         System.out.println( "Ingresa el segundo numero:" );
-        d2 = src.nextDouble();
+        Num2 = src.nextDouble();
     
-        result= d1+d2; 
+        result= Ejercicios.SumaDeDosNum(Num1,Num2) ;; 
     
         System.out.println( "La suma de estos numeros es:" + result);
         System.out.println( "Problema 3:Numero elevado al cuadrado." );
         System.out.println( "Ingresa un numero:" );
 
-        d1 = src.nextDouble();
+        Num1 = src.nextDouble();
 
-        result = d1*d1;
+        result = Ejercicios.NumeroAlCuadrado(Num1);
 
         System.out.println( "El numero al cuadrado es:" + result);
 
         System.out.println( "Problema 4:Euros a dolares." );
         System.out.println( "Ingresa el valor en euros:" );
 
-        d1 = src.nextDouble();
+        Num1 = src.nextDouble();
 
-        result = d1* 1.08;
+        result = Ejercicios.EuroADolar(Num1);
 
         System.out.println( "El valor en dolares es:" + result);
 
         System.out.println( "Problema 5:Area y perimetro cuadrado." );
         System.out.println( "Ingresa el lado de un cuadrado:" );
 
-        d1 = src.nextDouble();
+        Num1 = src.nextDouble();
 
-        result = d1 * d1;
+        result =Ejercicios.AreaCuadrado(Num1);
 
         System.out.println( "El area del cuadrado es:" + result);
 
-        result = d1*4;
+        result = Ejercicios.PerimetroCuadrado(Num1);
 
         System.out.println( "El perimetro del cuadrado es:" + result);
 
         System.out.println( "Problema 6:Area y volumen de un cilindro." );
         System.out.println( "Ingresa el radio del cilindro." );
 
-        d1 = src.nextDouble();
+        Num1 = src.nextDouble();
 
         System.out.println( "Ingresa la altura del cilindro." );
 
-        d2 = src.nextDouble();
+        Num2 = src.nextDouble();
 
-        result = (3.14 * (d1 * d1)) * d2;
+        result = Ejercicios.VolumenCilindro(Num1,Num2);
 
         System.out.println( "El volumen del cilindro es:" + result);
 
-        result = 2*3.14*d1*(d1 + d2);
+        result = Ejercicios.AreaCilindro(Num1,Num2);
 
         System.out.println( "El area del cilindro es:" + result);
 
         System.out.println( "Problema 7:Area y longitud de una circunferencia." );
         System.out.println( "Ingresa el radio de la circunferenia:");
 
-        d1 = src.nextDouble();
+        Num1 = src.nextDouble();
 
-        result = 3.14 * ( d1 * d1 );
+        result = Ejercicios.AreaCirc(Num1);
 
         System.out.println( "El area de la circunferencia es:" + result);
 
-        result = 2*3.14*d1;
+        result = Ejercicios.LongitudCirc(Num1);
 
         System.out.println( "La longitud de la circunferencia es:" + result);
 
         System.out.println( "Problema 8:Promedio." );
         System.out.println( "Ingresa el primer numero:");
-        d1 = src.nextDouble();
+        Num1 = src.nextDouble();
         System.out.println( "Ingresa el segundo numero:");
-        d2 = src.nextDouble();
+        Num2 = src.nextDouble();
         System.out.println( "Ingresa el tercer numero:");
-        d3 = src.nextDouble();
+        Num3 = src.nextDouble();
 
-        result= (d1+d2+d3)/3;
+        result= Ejercicios.PromedioTresNumeros(Num1,Num2,Num3);
 
         System.out.println( "El promedio de estos tres numeros es:" + result);
     }
